bc6b624aa9e0cbd23afe549c7ddf2eabd41cfaf5
Joana Amorim <joanasouzacontato@gmail.com>
2024-12-03 23:17:18 -0300

    desafio dia 3

---
+++ dia 3/script.js
@@ -0,0 +1,9 @@
+const numero = 145
+const texto = "Olá"
+const booleano = true
+const array = ["Joana", "Pedro", 74, 9, "Pão de Queijo"]
+
+console.log(typeof numero)
+console.log(typeof texto)
+console.log(typeof booleano)
+console.log(typeof array)
